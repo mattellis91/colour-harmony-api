32f40874df3a92668d269d3ffacdd4b93dec56be
Matt <mathew.ellis2@griffithuni.edu.au>
2021-03-11 20:26:45 +1000

    added additional methods to color calculator and test file for testing method outputs

--- colorCalculator.js
+++ colorCalculator.js
@@ -1,82 +1,12 @@
 let cc = {
     convertHextoRGB : (hex) => {
-        let rgb = [];
+        let rbgMap = {0 : 'r', 1 : 'g', 2: 'b'}
+        let rgb = {};
         for( var i = 0; i < hex.length; i+=2) {
             colorSegment = hex[i] + hex[i + 1];
-            rgb.push(parseInt(colorSegment,16));
+            rgb[rbgMap[i]] = parseInt(colorSegment,16);
         }
         return rgb;
-    },
-
-    convertHSVtoRGB : (h, s, v) => {
-        let r, g, b, i, f, p, q, t;
-        if (arguments.length === 1) {
-            s = h.s, v = h.v, h = h.h;
-        }
-        i = Math.floor(h * 6);
-        f = h * 6 - i;
-        p = v * (1 - s);
-        q = v * (1 - f * s);
-        t = v * (1 - (1 - f) * s);
-        switch (i % 6) {
-            case 0: r = v, g = t, b = p; break;
-            case 1: r = q, g = v, b = p; break;
-            case 2: r = p, g = v, b = t; break;
-            case 3: r = p, g = q, b = v; break;
-            case 4: r = t, g = p, b = v; break;
-            case 5: r = v, g = p, b = q; break;
-        }
-        return {
-            r: Math.round(r * 255),
-            g: Math.round(g * 255),
-            b: Math.round(b * 255)
-        };
-    },
-
-    convertRGBtoHex : (rgb) => {
-        let hex = "";
-        hex += rgb[0].toString(16);
-        hex += rgb[1].toString(16);
-        hex += rgb[2].toString(16);
-        return hex;
-    },
-
-    convertRGBtoHSV : (color) => {
-        var computedH = 0;
-        var computedS = 0;
-        var computedV = 0;
-
-        var r = color[0];
-        var g = color[1];
-        var b = color[2];
-
-        if ( r==null || g==null || b==null ||
-            isNaN(r) || isNaN(g)|| isNaN(b) ) {
-            console.log('Please enter numeric RGB values!');
-            return;
-        }
-        if (r<0 || g<0 || b<0 || r>255 || g>255 || b>255) {
-            console.log('RGB values must be in the range 0 to 255.');
-            return;
-        }
-
-        r=r/255; g=g/255; b=b/255;
-        var minRGB = Math.min(r,Math.min(g,b));
-        var maxRGB = Math.max(r,Math.max(g,b));
-
-        // Black-gray-white
-        if (minRGB==maxRGB) {
-            computedV = minRGB;
-            return [0,0,computedV];
-        }
-
-        // Colors other than black-gray-white:
-        var d = (r==minRGB) ? g-b : ((b==minRGB) ? r-g : b-r);
-        var h = (r==minRGB) ? 3 : ((b==minRGB) ? 1 : 5);
-        computedH = 60*(h - d/(maxRGB - minRGB));
-        computedS = (maxRGB - minRGB)/maxRGB;
-        computedV = maxRGB;
-        return [Math.round(computedH),Math.round(computedS*100),Math.round(computedV*100)];
     },
 
     convertHSVtoRBG : (hsvColor) => {
@@ -150,12 +80,145 @@ let cc = {
                 b = q;
         }
         
-        return [
-            Math.round(r * 255), 
-            Math.round(g * 255), 
-            Math.round(b * 255)
-        ];
+        return {
+         'r' :  Math.round(r * 255), 
+         'g' :  Math.round(g * 255), 
+         'b' :  Math.round(b * 255)
+        };
 
+    },
+
+    pSBC :(p,c0,c1,l) => {
+        let r,g,b,P,f,t,h,i=parseInt,m=Math.round,a=typeof(c1)=="string";
+        if(typeof(p)!="number"||p<-1||p>1||typeof(c0)!="string"||(c0[0]!='r'&&c0[0]!='#')||(c1&&!a))return null;
+        if(!this.pSBCr)this.pSBCr=(d)=>{
+            let n=d.length,x={};
+            if(n>9){
+                [r,g,b,a]=d=d.split(","),n=d.length;
+                if(n<3||n>4)return null;
+                x.r=i(r[3]=="a"?r.slice(5):r.slice(4)),x.g=i(g),x.b=i(b),x.a=a?parseFloat(a):-1
+            }else{
+                if(n==8||n==6||n<4)return null;
+                if(n<6)d="#"+d[1]+d[1]+d[2]+d[2]+d[3]+d[3]+(n>4?d[4]+d[4]:"");
+                d=i(d.slice(1),16);
+                if(n==9||n==5)x.r=d>>24&255,x.g=d>>16&255,x.b=d>>8&255,x.a=m((d&255)/0.255)/1000;
+                else x.r=d>>16,x.g=d>>8&255,x.b=d&255,x.a=-1
+            }return x};
+        h=c0.length>9,h=a?c1.length>9?true:c1=="c"?!h:false:h,f=this.pSBCr(c0),P=p<0,t=c1&&c1!="c"?this.pSBCr(c1):P?{r:0,g:0,b:0,a:-1}:{r:255,g:255,b:255,a:-1},p=P?p*-1:p,P=1-p;
+        if(!f||!t)return null;
+        if(l)r=m(P*f.r+p*t.r),g=m(P*f.g+p*t.g),b=m(P*f.b+p*t.b);
+        else r=m((P*f.r**2+p*t.r**2)**0.5),g=m((P*f.g**2+p*t.g**2)**0.5),b=m((P*f.b**2+p*t.b**2)**0.5);
+        a=f.a,t=t.a,f=a>=0||t>=0,a=f?a<0?t:t<0?a:a*P+t*p:0;
+        if(h)return"rgb"+(f?"a(":"(")+r+","+g+","+b+(f?","+m(a*1000)/1000:"")+")";
+        else return"#"+(4294967296+r*16777216+g*65536+b*256+(f?m(a*255):0)).toString(16).slice(1,f?undefined:-2)
+    },
+
+    convertRGBtoHex : (rgb) => {
+        let hex = "";
+        hex += rgb.r.toString(16);
+        hex += rgb.g.toString(16);
+        hex += rgb.b.toString(16);
+        return hex;
+    },
+
+    rgbToHsl: (r, g, b) => {
+        r /= 255, g /= 255, b /= 255;
+        var max = Math.max(r, g, b), min = Math.min(r, g, b);
+        var h, s, l = (max + min) / 2;
+        if(max == min){
+          h = s = 0; // achromatic
+        }else{
+          var d = max - min;
+          s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+          switch(max){
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+          }
+          h /= 6;
+        }
+        return({ h:h, s:s, l:l });
+      },
+
+      hslToRgb: (h, s, l) => {
+        var r, g, b;
+        if(s == 0){
+            r = g = b = l; // achromatic
+        }else{
+            function hue2rgb(p, q, t){
+            if(t < 0) t += 1;
+            if(t > 1) t -= 1;
+            if(t < 1/6) return p + (q - p) * 6 * t;
+            if(t < 1/2) return q;
+            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+            return p;
+            }
+            var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+            var p = 2 * l - q;
+            r = hue2rgb(p, q, h + 1/3);
+            g = hue2rgb(p, q, h);
+            b = hue2rgb(p, q, h - 1/3);
+        }
+        return({
+            r:Math.round(r * 255),
+            g:Math.round(g * 255),
+            b:Math.round(b * 255),
+        });
+    },
+
+    shiftHue(rgb, degree) {
+
+        const r = rbg.r
+        const g = rbg.g
+        const b = rbg.b
+
+        var hsl = rgbToHSL(r,g,b);
+        hsl.h += degree;
+        if (hsl.h > 360) {
+            hsl.h -= 360;
+        }
+        else if (hsl.h < 0) {
+            hsl.h += 360;
+        }
+        return hslToRGB(hsl);
+    },
+
+    convertRGBtoHSV : (rgb) => {
+        var computedH = 0;
+        var computedS = 0;
+        var computedV = 0;
+
+        var r = rgb.r;
+        var g = rgb.g;
+        var b = rgb.b;
+
+        if ( r==null || g==null || b==null ||
+            isNaN(r) || isNaN(g)|| isNaN(b) ) {
+            console.log('Please enter numeric RGB values!');
+            return;
+        }
+        if (r<0 || g<0 || b<0 || r>255 || g>255 || b>255) {
+            console.log('RGB values must be in the range 0 to 255.');
+            return;
+        }
+
+        r=r/255; g=g/255; b=b/255;
+        var minRGB = Math.min(r,Math.min(g,b));
+        var maxRGB = Math.max(r,Math.max(g,b));
+
+        // Black-gray-white
+        if (minRGB==maxRGB) {
+            computedV = minRGB;
+            return [0,0,computedV];
+        }
+
+        // Colors other than black-gray-white:
+        var d = (r==minRGB) ? g-b : ((b==minRGB) ? r-g : b-r);
+        var h = (r==minRGB) ? 3 : ((b==minRGB) ? 1 : 5);
+        computedH = 60*(h - d/(maxRGB - minRGB));
+        computedS = (maxRGB - minRGB)/maxRGB;
+        computedV = maxRGB;
+        return [Math.round(computedH),Math.round(computedS*100),Math.round(computedV*100)];
     },
 
     mod : (x, n) => { return ((x % n) + n) % n } ,
@@ -178,6 +241,66 @@ let cc = {
             "complementaryHex" : complementaryHex,
             "complementaryHSV" : complementaryHSV
         }
+    },
+
+    // Returns a single rgb color interpolation between given rgb color
+    // based on the factor given; via https://codepen.io/njmcode/pen/axoyD?editors=0010
+    interpolateColor : (color1, color2, factor) => {
+        if (arguments.length < 3) { 
+            factor = 0.5; 
+        }
+        var result = color1.slice();
+        for (var i = 0; i < 3; i++) {
+            result[i] = Math.round(result[i] + factor * (color2[i] - color1[i]));
+        }
+        return result;
+    },
+    // My function to interpolate between two colors completely, returning an array
+    interpolateColors : (color1, color2, steps) => {
+        var stepFactor = 1 / (steps - 1),
+            interpolatedColorArray = [];
+
+        color1 = color1.match(/\d+/g).map(Number);
+        color2 = color2.match(/\d+/g).map(Number);
+
+        for(var i = 0; i < steps; i++) {
+            interpolatedColorArray.push(interpolateColor(color1, color2, stepFactor * i));
+        }
+
+        return interpolatedColorArray;
+    },
+
+    darken : (color, percentage) => {
+        return pSBC(percentage,color); 
+    },
+
+    lighten : (color, percentage) => {
+        return psCB(percentage * -1, color)
+    },
+
+    invertColor : (hex) => {
+        if (hex.indexOf('#') === 0) {
+            hex = hex.slice(1);
+        }
+        // convert 3-digit hex to 6-digits.
+        if (hex.length === 3) {
+            hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+        }
+        if (hex.length !== 6) {
+            throw new Error('Invalid HEX color.');
+        }
+        // invert color components
+        var r = (255 - parseInt(hex.slice(0, 2), 16)).toString(16),
+            g = (255 - parseInt(hex.slice(2, 4), 16)).toString(16),
+            b = (255 - parseInt(hex.slice(4, 6), 16)).toString(16);
+        // pad each with zeros and return
+        return '#' + padZero(r) + padZero(g) + padZero(b);
+    },
+    
+    padZero : (str, len) => {
+        len = len || 2;
+        var zeros = new Array(len).join('0');
+        return (zeros + str).slice(-len);
     },
 
     calculateSplitComplementary : (base) => {
---
+++ test.js
@@ -0,0 +1,3 @@
+let cc = require("./colorCalculator") 
+
+console.log(cc.pSBC(-0.1,"rgb(255,0,0)"));
