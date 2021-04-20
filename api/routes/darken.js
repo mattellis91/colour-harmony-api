c9169ac49afc8ceca785d6879b5ada5145703ebb
Matt Ellis <mathew.ellis2@griffithuni.edu.au>
2021-04-20 18:29:18 +1000

    updated daken route

--- api/routes/darken.js
+++ api/routes/darken.js
@@ -12,19 +12,22 @@ router.get('/',(req, res, next) => {
 const getDarkenResponse = (req,res,method) => {
     const params = req.query;
     const color = util.getColorValue(params);
+    const percentage = util.getPercentage(params);
     if(!color) {
         return res.send(response.buildErrorResponse(req,2,response.NO_COLOR_PROVIDED));
     }
+    validPercentage = validator.isValidPercentage(percentage);
+    if(!validPercentage.success) {
+        return res.send(response.buildErrorResponse(req,2,validPercentage.error));
+    }
     let validCheck;
     let rgb;
-    return res.send(color);
     switch(color.type) {
         case 'hex': 
             validCheck = validator.isValidHex(color.value);
             if(!validCheck.success) {
                 //return res.send(response.buildErrorResponse(req,2,validCheck.error));
             }
-            return res.send(response.buildSuccessResponse(req, harmonyMethod(color.value, color.value)));
         case 'hsl': 
             validCheck = validator.isValidHsl(color.value);
             if(!validCheck.success) {
@@ -32,23 +35,21 @@ const getDarkenResponse = (req,res,method) => {
             }
             const hslObj = {'h' : color.value[0], 's': color.value[1], 'l': color.value[2]};
             rgb = colorConverter.hslToRgb(Number.parseInt(hslObj.h) / 360, Number.parseInt(hslObj.s) / 100, Number.parseInt(hslObj.l) / 100);
-            //return res.send(response.buildSuccessResponse(req, harmonyMethod(rgb, hslObj)));
         case 'hsv': 
             validCheck = validator.isValidHsv(color.value);
             if(!validCheck.success) {
                 return res.send(response.buildErrorResponse(req,2,validCheck.error));
             }
-            const hsvObj = {'h' : color.value[0], 's': color.value[1], 'v': color.value[2]};
-            rgb = colorConverter.hsvToRgb(hsvObj.h / 360,hsvObj.s / 100,hsvObj.v / 100);
-            //return res.send(response.buildSuccessResponse(req,harmonyMethod(rgb, hsvObj)));
+        const hsvObj = {'h' : color.value[0], 's': color.value[1], 'v': color.value[2]};
+        rgb = colorConverter.hsvToRgb(hsvObj.h / 360,hsvObj.s / 100,hsvObj.v / 100);
         case 'rgb': 
             validCheck = validator.isValidRgb(color.value);
             if(!validCheck.success) {
                 return res.send(response.buildErrorResponse(req,2,validCheck.error));
             }
-            rgb = {'r' : color.value[0], 'g': color.value[1], 'b': color.value[2]};
-            //return res.send(response.buildSuccessResponse(req,harmonyMethod(rgb,rgb)));
+            const result = colorCalculator.darken(color,Number.parseInt(percentage) / 100);
+            return res.send(response.buildSuccessResponse(req,result));
     }
 }
-
+//
 module.exports = router;
